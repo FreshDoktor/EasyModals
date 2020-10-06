af703363b0352d77c0454b8b6c1aa33f9d4989c1
Leon <freshdoktor@freshgaming.eu>
2020-10-06 21:19:30 +0200

    fix: add back em.module in public-api

--- projects/easy-modals/src/public-api.ts
+++ projects/easy-modals/src/public-api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/easy-modals.service';
+export * from './lib/easy-modals.module';
 export * from './lib/confirmation-modal/confirmation-modal-config';
 export * from './lib/confirmation-modal/confirmationmodal-ref';
 export * from './lib/custom-modal/custom-modal-ref';
